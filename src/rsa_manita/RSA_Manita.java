83575baa972ecc3cd4d0f0148f1de9b73904f1e1
EAlonsoBer <Alonso.Berruecos.Emmanuel@gmail.com>
2023-11-19 00:59:25 -0600

    cambio

---
+++ src/rsa_manita/RSA_Manita.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rsa_manita;
+
+import GUI.MENU;
+
+/**
+ *
+ * @author User
+ */
+public class RSA_Manita {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MENU menu = new MENU();
+        menu.setVisible(true);
+        menu.setLocationRelativeTo(null);
+    }
+    
+}
